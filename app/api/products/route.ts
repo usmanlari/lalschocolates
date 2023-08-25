b2be7c4900db43c00c344eb20c77da3c051538e9
usmanlari <usmanlari@hotmail.com>
2023-08-26 00:16:34 +0500

    Revalidating app/api/products/route.ts every second

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -2,6 +2,7 @@ import connectMongoDB from "@/lib/mongodb";
 import Product from "@/models/product";
 import { NextResponse } from "next/server";
 
+export const revalidate = 1;
 export async function GET() {
   await connectMongoDB();
   const products = await Product.find();
